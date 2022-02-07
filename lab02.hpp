dbe26c657277b02789711203bdc172312f62b523
Mohamad AlMdfaa <mohamad.almdfaa@gmail.com>
2022-02-07 22:32:46 +0300

    Initial commit

---
+++ lab02.hpp
@@ -0,0 +1,26 @@
+#pragma once // prevents multiple definitions
+
+#include "ex01.hpp"
+#include "ex02.hpp"
+
+// Exercise 0a: implements function 'add', that adds two numbers
+int add(int a, int b);
+
+// Exercise 0b: implements function 'mul', that multiplies two numbers
+int mul(int a, int b);
+
+// ===============
+// implementations
+
+// Exercise 0a: implements function 'add', that adds two numbers
+int add(int a, int b)
+{
+   return 0; // TODO: fix
+}
+
+// Exercise 0b: implements function 'mul', that multiplies two numbers
+int mul(int a, int b)
+{
+   return 0; // TODO: fix
+}
+
